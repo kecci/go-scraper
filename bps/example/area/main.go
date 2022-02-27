783a81e171427ea35bfd3a79db1f569e38b1dea0
kecci <kecci@machtwatch.co.id>
2022-02-27 21:19:39 +0700

    revert uncomment

--- bps/example/area/main.go
+++ bps/example/area/main.go
@@ -15,7 +15,7 @@ func main() {
 	var err error
 	var b []byte
 	var areaRes []bps.AreaModel
-	// var searchRes []bps.SearchModel
+	var searchRes []bps.SearchModel
 
 	log.Println("################## BPS Province Data: ##################")
 	areaRes, err = bps.GetProvinces()
@@ -25,37 +25,37 @@ func main() {
 	b, _ = json.Marshal(areaRes)
 	log.Println(string(b))
 
-	// log.Println("################## BPS Regency Data: ##################")
-	// areaRes, err = bps.GetRegencies(areaRes[0].KodeBps)
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// }
-	// b, _ = json.Marshal(areaRes)
-	// log.Println(string(b))
+	log.Println("################## BPS Regency Data: ##################")
+	areaRes, err = bps.GetRegencies(areaRes[0].KodeBps)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	b, _ = json.Marshal(areaRes)
+	log.Println(string(b))
 
-	// log.Println("################## BPS District Data: ##################")
-	// areaRes, err = bps.GetDistricts(areaRes[0].KodeBps)
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// }
-	// b, _ = json.Marshal(areaRes)
-	// log.Println(string(b))
+	log.Println("################## BPS District Data: ##################")
+	areaRes, err = bps.GetDistricts(areaRes[0].KodeBps)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	b, _ = json.Marshal(areaRes)
+	log.Println(string(b))
 
-	// log.Println("################## BPS Village Data: ##################")
-	// areaRes, err = bps.GetVillages(areaRes[0].KodeBps)
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// }
-	// b, _ = json.Marshal(areaRes)
-	// log.Println(string(b))
+	log.Println("################## BPS Village Data: ##################")
+	areaRes, err = bps.GetVillages(areaRes[0].KodeBps)
+	if err != nil {
+		log.Println(err.Error())
+	}
+	b, _ = json.Marshal(areaRes)
+	log.Println(string(b))
 
-	// log.Println("################## BPS Search Area Data: ##################")
-	// searchRes, err = bps.SearchArea("jakarta")
-	// if err != nil {
-	// 	log.Println(err.Error())
-	// }
-	// b, _ = json.Marshal(searchRes)
-	// log.Println(string(b))
+	log.Println("################## BPS Search Area Data: ##################")
+	searchRes, err = bps.SearchArea("jakarta")
+	if err != nil {
+		log.Println(err.Error())
+	}
+	b, _ = json.Marshal(searchRes)
+	log.Println(string(b))
 
 	// Elapesd
 	elapsed := time.Since(start)
